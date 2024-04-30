303b7bf8d43c25874cac5e4bffc4d4f6c40b7c38
anshu <anshu@merchantech.com>
2024-04-30 15:43:54 +0530

    something

--- app/src/main/java/com/example/composeroom/room/ContactDatabase.kt
+++ app/src/main/java/com/example/composeroom/room/ContactDatabase.kt
@@ -6,5 +6,6 @@ import androidx.room.RoomDatabase
 @Database(entities = [Contact::class], version = 1)
 abstract class ContactDatabase : RoomDatabase() {
 
+    abstract fun contactDao(): ContactDao
 
 }
